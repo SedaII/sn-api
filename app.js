e07013d22b685d48b602b3ad2e1519cc82166bfa
SedaII <i.messnaoui@gmail.com>
2022-05-14 14:30:21 +0200

    add cors headers

--- app.js
+++ app.js
@@ -45,13 +45,14 @@ app.use(expressSession({
 	store: sequelizeSessionStore,
 	resave: false,
 	saveUninitialized: false,
-  /* cookie: {
-    maxAge: 5*1000
-  } */
+  cookie: {
+    sameSite: "strict"
+  }
 }));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
   next();
